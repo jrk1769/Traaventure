4adb8ed57a727609810b1b7ed89a385ac5743ff1
Shrey Shah <shreyshah67@gmail.com>
2021-04-11 22:12:31 +0530

    Changes for gallery file

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,37 +1,46 @@
-import React from 'react'
-import useFetch from '../useFetch/useFetch';
-import Images from './Images';
-import Menu from './Menu';
+import React from "react";
+import useFetch from "../useFetch/useFetch";
+import Images from "./Images";
+import Menu from "./Menu";
 import "./Gallery.css";
 
 /**
-* @author
-* @function Gallery
-**/
+ * @author
+ * @function Gallery
+ **/
 
 const Gallery = (props) => {
-  const { data: images, error: imagesErr, isLoading: imagesLoad } = useFetch('http://localhost:8000/images');
-  const { data: places, error: placesErr, isLoading: placesLoad } = useFetch('http://localhost:8000/places');
+  const { data: images, error: imagesErr, isLoading: imagesLoad } = useFetch(
+    "http://localhost:8000/images"
+  );
+  const { data: places, error: placesErr, isLoading: placesLoad } = useFetch(
+    "http://localhost:8000/places"
+  );
+
+  const handleGalleryFilter = (categoryName) => {
+    console.log(categoryName);
+    images = images.filter((image) => {
+      return image.categoryName === categoryName;
+    });
+  };
 
   return (
     <>
-      <div className="backgrnd">
-
-      </div>
+      <div className="backgrnd"></div>
       <section className="gallery-container">
         {placesErr && <div>{placesErr}</div>}
         {placesLoad && <div>Loading...</div>}
-        {places && <Menu places={places} />}
+        {places && (
+          <Menu handleGalleryFilter={handleGalleryFilter} places={places} />
+        )}
         <div className="gallery">
           {imagesErr && <div>{imagesErr}</div>}
           {imagesLoad && <div>Loading...</div>}
           {images && <Images images={images} />}
         </div>
       </section>
-
     </>
-  )
+  );
+};
 
-}
-
-export default Gallery
+export default Gallery;
